b24522870bda90175f3ccdba3b60dfb8a6cd91ca
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2025-06-20 20:21:07 +0800

    Update doinst.sh

--- flatpak-xdg-utils/doinst.sh
+++ flatpak-xdg-utils/doinst.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 ln -s /usr/lib64/flatpak-xdg-utils/flatpak-spawn /usr/bin/flatpak-spawn
+ln -s /usr/lib64/flatpak-xdg-utils/xdg-open /usr/bin/flatpak-xdg-open
+ln -s /usr/lib64/flatpak-xdg-utils/xdg-email /usr/bin/flatpak-xdg-email
